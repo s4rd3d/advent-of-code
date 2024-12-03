75af416e33b25c9dab7c0c52a9653575065f55cd
David Sandor <dsandor@cursorinsight.com>
2024-12-03 09:07:53 +0100

    Update 2024 / day 3 solution

--- 2024/3/script.exs
+++ 2024/3/script.exs
@@ -9,35 +9,15 @@ defmodule Day03 do
     |> Enum.map(fn line ->
       line
       |> Enum.map(&String.to_integer/1)
-      |> Enum.reduce(1, &(&1 * &2))
+      |> Enum.product()
     end)
     |> Enum.sum()
   end
 
   def part_2(input) do
-    ~r/mul\((\d{1,3}),(\d{1,3})\)|(do\(\))|(don't\(\))/
-    |> Regex.scan(input, capture: :all_but_first)
-    |> Enum.map(fn line ->
-      line
-      |> Enum.filter(&(&1 != ""))
-    end)
-    |> Enum.reduce(
-      {:enabled, 0},
-      fn
-        ["don't()"], {_, acc} ->
-          {:disabled, acc}
-
-        ["do()"], {_, acc} ->
-          {:enabled, acc}
-
-        [e1, e2], {:enabled, acc} ->
-          {:enabled, acc + String.to_integer(e1) * String.to_integer(e2)}
-
-        _, {:disabled, acc} ->
-          {:disabled, acc}
-      end
-    )
-    |> elem(1)
+    input
+    |> String.replace(~r/don't\(\)[\s\S]+?(do\(\)|$)/, "")
+    |> part_1()
   end
 
 end
